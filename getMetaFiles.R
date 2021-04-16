66d5a1d5392d40078d696aae7de4a26395e897fd
Nicholas D. Socci <soccin@mskcc.org>
2021-04-15 23:01:33 -0400

    V1 getMetaFiles.R

--- getMetaFiles.R
+++ getMetaFiles.R
@@ -15,4 +15,14 @@ VERSION="v1.0.1"
 SDIR=dirname(ndsTools::thisFile())
 source(file.path(SDIR,"tools.R"))
 ####################################################################################
+DATE <- function(){gsub("-", "", Sys.Date())}
 ####################################################################################
+require(tidyverse)
+####################################################################################
+
+dx=map(args,read_csv) %>% map(mutate,Sample=gsub(".tif","",`Image File Name`)) %>% bind_rows
+
+markerKey=tibble(ColNames=grep(" Positive Classification",colnames(dx),value=T),Marker="",Type="") %>%
+    mutate(Marker=gsub(" .*","",ColNames))
+
+write_csv(markerKey,paste0("markerKey_",DATE(),".csv"))
